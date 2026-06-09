9d57ed12871bd9a7a7472a01080f433f03672ab7
k3s-forge <k3s-forge@zvip.eu.org>
2026-06-09 20:30:26 +0800

    feat: wire firewall ACL rules from agent config to BPF maps

--- cmd/nomad-p2p/agent.go
+++ cmd/nomad-p2p/agent.go
@@ -246,10 +246,60 @@ func (a *Agent) loadBPF() error {
 				a.maps.DefaultPolicy.Update(key, val, 0)
 			}
 			log.Printf("[agent] firewall BPF loaded")
+			a.loadACLsFromConfig()
 		}
 	}
 
 	return nil
+}
+
+func (a *Agent) loadACLsFromConfig() {
+	if !a.cfg.FirewallEnabled || a.maps.ACLMap == nil {
+		return
+	}
+
+	// Set default policy
+	if a.maps.DefaultPolicy != nil {
+		key := uint32(0)
+		val := uint8(0) // deny by default
+		if a.cfg.DefaultPolicy == "allow" {
+			val = 1
+		}
+		a.maps.DefaultPolicy.Update(key, val, 0)
+		log.Printf("[agent] firewall default policy: %s", a.cfg.DefaultPolicy)
+	}
+
+	// Load IP-level ACLs
+	for _, srcIP := range a.cfg.AllowedSources {
+		ip := net.ParseIP(srcIP).To4()
+		if ip == nil {
+			continue
+		}
+		ipU := *(*uint32)(unsafe.Pointer(&ip[0]))
+		allow := uint8(1)
+		a.maps.ACLMap.Update(ipU, allow, 0)
+		log.Printf("[agent] ACL: allow %s", srcIP)
+	}
+
+	// Load port-level ACLs
+	for _, rule := range a.cfg.AllowedPorts {
+		srcIP := net.ParseIP(rule.SourceIP).To4()
+		if srcIP == nil {
+			continue
+		}
+		srcU := *(*uint32)(unsafe.Pointer(&srcIP[0]))
+		portKey := (uint64(srcU) << 32) | uint64(rule.Port)
+		var allow uint8
+		if rule.Allow {
+			allow = 1
+		}
+		a.maps.PortACLMap.Update(portKey, allow, 0)
+		log.Printf("[agent] port ACL: %s %s/%d -> %v",
+			rule.Protocol, rule.SourceIP, rule.Port, rule.Allow)
+	}
+
+	log.Printf("[agent] loaded %d IP rules, %d port rules",
+		len(a.cfg.AllowedSources), len(a.cfg.AllowedPorts))
 }
 
 // --- STUN ---
